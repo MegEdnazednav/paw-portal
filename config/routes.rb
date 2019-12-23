7c69b050b10ebaec2890c5b78d60852d86d41f8d
Meg Vandezande <meg.vandezande@gmail.com>
2019-12-23 13:33:16 +0100

    add animal model, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to "animals#index"
+
+  resources :animals, except: :index
 end
---
+++ db/migrate/20191223122936_create_animals.rb
@@ -0,0 +1,13 @@
+class CreateAnimals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :animals do |t|
+      t.string :type
+      t.string :name
+      t.integer :age
+      t.text :description
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
