ef4c2835091652212f2bf589a9f52a241c6a4fa8
Meg Vandezande <meg.vandezande@gmail.com>
2020-01-02 23:06:49 +0100

    commenting todo's

--- app/javascript/react_frontend/redux/actions/deleteAnimalActions.js
+++ app/javascript/react_frontend/redux/actions/deleteAnimalActions.js
@@ -3,13 +3,13 @@ export const DELETE_ANIMAL = 'DELETE_ANIMAL';
 function deleteAnimal(id) {
   const animal = fetch(`/api/v1/animals/${id}`, {
     method: 'DELETE',
-    headers: { 'Content-Type': 'application/json',
+    headers: { 'Content-Type': 'application/json', //#TODO Cleanup headers
                'Accept': 'application/json',
                'X-Requested-With': 'XMLHttpRequest',
                'Cache-Control': 'no-cache',
                'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
              },
-    body: JSON.stringify({id: `${id}`})
+    body: JSON.stringify({id: `${id}`}) //#TODO Body not necessary
   }).then(response => response.json())
     return {
       type: DELETE_ANIMAL,
