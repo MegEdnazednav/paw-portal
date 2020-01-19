771cb66a10002a06c157f26462bde55b6cbd2efc
Meg Vandezande <meg.vandezande@gmail.com>
2020-01-19 18:08:27 +0100

    First version of uploading image with active storage and returning url

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,3 @@
 class Animal < ApplicationRecord
+  has_one_attached :primary_image
 end
--- app/serializers/animal_serializer.rb
+++ app/serializers/animal_serializer.rb
@@ -1,3 +1,12 @@
 class AnimalSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :kind, :description
+
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :age, :kind, :description, :primary_image_url
+
+  def primary_image_url
+    return unless object.primary_image.attached?
+
+    rails_blob_path(object.primary_image, only_path: true)
+  end
 end
